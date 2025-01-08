5c3812978c4a6e97474b466a0584d5d1115b63bb
namdev <namnkq2603@gmail.com>
2025-01-08 13:12:00 +0700

    chore: refactor folder structure

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <h1 className='text-4xl text-red-500'>Food Trove App</h1>
+}
+
+export default HomePage
---
+++ src/components/common/index.ts
@@ -0,0 +1,4 @@
+import Header from '@/components/common/header'
+import Footer from '@/components/common/footer'
+
+export { Header, Footer }
