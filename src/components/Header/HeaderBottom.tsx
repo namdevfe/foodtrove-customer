c64b0ab11268f421e82b9a226f89fd1c3f6e2ec0
namdev <namnkq2603@gmail.com>
2025-01-13 18:39:56 +0700

    fix(layout): add backdrop-filter blur header

--- src/components/Header/HeaderBottom.tsx
+++ src/components/Header/HeaderBottom.tsx
@@ -3,39 +3,41 @@ import Image from 'next/image'
 
 const HeaderBottom = () => {
   return (
-    <div className='container h-full py-6 flex items-center justify-between'>
-      <div className='flex items-center gap-4'>
-        <ToggleMenuButton />
-        <Logo />
-      </div>
-
-      <Menu />
-      <Search />
-      <div className='flex'>
-        <div className='cursor-pointer p-[6px] shrink-0'>
-          <Image
-            src='/assets/images/icon-search-black.svg'
-            alt='icon-search'
-            width={20}
-            height={20}
-          />
-        </div>
-        <div className='cursor-pointer p-[6px] shrink-0'>
-          <Image
-            src='/assets/images/icon-cart.svg'
-            alt='icon-cart'
-            width={22}
-            height={20}
-          />
+    <div className='backdrop-blur-md'>
+      <div className='container h-full py-6 flex items-center justify-between'>
+        <div className='flex items-center gap-4'>
+          <ToggleMenuButton />
+          <Logo />
         </div>
 
-        <div className='cursor-pointer p-[6px] shrink-0'>
-          <Image
-            src='/assets/images/icon-user.svg'
-            alt='icon-user'
-            width={20}
-            height={20}
-          />
+        <Menu />
+        <Search />
+        <div className='flex'>
+          <div className='cursor-pointer p-[6px] shrink-0'>
+            <Image
+              src='/assets/images/icon-search-black.svg'
+              alt='icon-search'
+              width={20}
+              height={20}
+            />
+          </div>
+          <div className='cursor-pointer p-[6px] shrink-0'>
+            <Image
+              src='/assets/images/icon-cart.svg'
+              alt='icon-cart'
+              width={22}
+              height={20}
+            />
+          </div>
+
+          <div className='cursor-pointer p-[6px] shrink-0'>
+            <Image
+              src='/assets/images/icon-user.svg'
+              alt='icon-user'
+              width={20}
+              height={20}
+            />
+          </div>
         </div>
       </div>
     </div>
